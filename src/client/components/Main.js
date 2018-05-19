8ca250abed12313ce17bcc58efbc50a3c8fa7642
Nico <nico_hagemann@web.de>
2018-05-20 00:21:20 +0200

    fix: login and register without navigation

--- src/client/components/Main.js
+++ src/client/components/Main.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import NoteList from "./NoteList";
 import AddNote from "../containers/AddNote";
 import VisibleNoteList from "../containers/VisibleNoteList";
+import VisibleNavigationList from '../containers/VisibleNavigationList';
+import VisibleCategoryList from '../containers/VisibleCategoryList';
 
 const Wrapper = styled.main`
   background: pink;
@@ -12,14 +14,30 @@ const Wrapper = styled.main`
   width: calc(100% - 256px);
   overflow-x: hidden;
   box-sizing: border-box;
-`
+`;
+
+const Navbar = styled.div`
+    position: absolute;
+    top: 56px;
+    left: 0;
+    width: 256px;
+    background: #343a40;
+    height: calc(100vh - 56px);
+    box-sizing: border-box;
+`;
 
 const Main = () => (
+  <div>
+  <Navbar>
+		  <VisibleNavigationList/>
+		  <VisibleCategoryList/>
+	  </Navbar>
   <Wrapper >
       <AddNote/>
           <VisibleNoteList/>
 
   </Wrapper>
+  </div>
 )
 
 export default Main;
